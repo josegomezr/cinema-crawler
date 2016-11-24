78e6796298bfa03428147e820944efdf86649c49
Jose D. Gomez R <1josegomezr@gmail.com>
2016-11-24 14:31:23 -0400

    MovieTime listo

--- crawlers/__init__.py
+++ crawlers/__init__.py
@@ -2,3 +2,4 @@ from .cinerama import CineramaCrawler
 from .cinepolis import CinepolisCrawler
 from .cinestar_multicine import CinestarCrawler
 from .cineplanet import CineplanetCrawler
+from .movietime import MovieTimeCrawler
--- runner.py
+++ runner.py
@@ -2,7 +2,7 @@ import crawlers
 import json
 
 if __name__ == '__main__':
-  crawler = crawlers.CineplanetCrawler()
+  crawler = crawlers.MovieTimeCrawler()
   crawler.getTheaters()
   crawler.getMovies()
   js = json.dump(crawler.model.toJSON(), open('%s.json' % crawler.name, 'w'))
