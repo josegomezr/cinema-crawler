8265c03d9edfb41558ddec3607d7761436edba0f
Jose Gomez <1josegomezr@gmail.com>
2017-01-03 16:44:59 -0400

    añadidas facilidades para loggers, ahora se entiende mejor el orden de las consultas HTTP y el logger tiene mas informacion

--- crawlers/base.py
+++ crawlers/base.py
@@ -2,18 +2,22 @@
 from bs4 import BeautifulSoup
 import requests
 import logging
-i = 1
+from datetime import datetime
 
 class BaseCrawler(object):
   '''base class for crawlers'''
   def __init__(self, name):
     '''@param name string crawler name'''
     self.name = name
-    self.log('init')
+    self.request_count = 1
+    self.logger = logging.getLogger(self.name)
+    self.logger.setLevel(logging.DEBUG)
+    self.logger.addHandler(logging.FileHandler('./tmp/log-%s.log' % (self.name), mode='a', encoding='utf8'))
+    self.logger.debug('init')
 
   def log(self, msg):
     '''@param msg mixed var to log'''
-    print("LOG[%s]:" % self.name, msg)
+    self.logger.info(msg)
 
   def getTheaters():
     '''This will fetch all theaters for a chain'''
@@ -31,16 +35,16 @@ class BaseCrawler(object):
   def makeBS4(self, html_doc):
     '''This make a bs4 object from a string
     @returns bs4.BeautifulSoup'''
-    global i
-    open('./tmp/%s-%i.log' % (self.name, i), 'w').write(str(html_doc))
-    i = i +1
+    i = self.request_count
+    open('./tmp/%s_%s_%i.request' % (self.name, datetime.now().strftime("%Y-%m-%dT%H-%I-%S"), i), 'w').write(str(html_doc))
+    self.request_count = self.request_count +1
     return BeautifulSoup(html_doc, 'html.parser')
   
   def doRequest(self, url, **kwargs):
     '''This make an http request and return a 
     @returns requests.Request'''
 
-    self.log('sending-http-request')
+    self.log('sending-http-request #[%d]\nURL:%s\nARGS:%s\n' % (self.request_count, url, str(kwargs)))
 
     retries = 10 # max retries for http request
     method = kwargs.get('method')
@@ -59,6 +63,6 @@ class BaseCrawler(object):
         self.log('successful-http-request')
         return response
       except (requests.exceptions.ContentDecodingError, requests.exceptions.ConnectionError):
-        self.log('failed-http-request')
+        self.log('failed-http-request retrying')
         retries = retries -1
     raise Exception("Network Error")
